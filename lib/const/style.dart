6e078af98eb82c4866e4cccab7d244eb2af8965d
Erhabor-Fona <osaofonaerhabor@gmail.com>
2023-01-11 08:50:41 +0100

    [Update]--include new colour in style

--- lib/const/style.dart
+++ lib/const/style.dart
@@ -1,3 +1,15 @@
 import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:google_fonts/google_fonts.dart';
 
 const Color kGrayText = Color(0xff575757);
+const Color kIcon4Color = Color(0xff37EFBA);
+
+
+TextStyle kTextFieldLabel = GoogleFonts.roboto(
+    fontStyle: FontStyle.normal,
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+    color: kIcon4Color);
--- lib/ui/components/txt.fields.dart
+++ lib/ui/components/txt.fields.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:form_validator/form_validator.dart';
 
 class TextAuthTextField extends StatelessWidget {
   final String? label;
